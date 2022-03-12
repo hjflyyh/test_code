516d156def3a68b190020962b22edfbb118e8465
Kyohi <hjflyyh@gmail.com>
2022-03-12 16:10:44 -0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,49 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  
 </template>
 
+<script>
+import { defineComponent } from '@vue/composition-api'
+import ApolloClient from "apollo-boost";
+import VueApollo from "vue-apollo";
+Vue.use(VueApollo);
+
+import CyberConnect, { Env, Blockchain } from "@cyberlab/cyberconnect";
+
+const cyberConnect = new CyberConnect({
+  namespace: "CyberConnect",
+  chain: Blockchain.ETH,
+  provider: provider,
+});
+
+const apolloClient = new ApolloClient({
+  uri: "http://localhost:4000/graphql"
+});
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient
+});
+
+new Vue({
+  router,
+  apolloProvider,
+  render: h => h(App)
+}).$mount('#app')
+
+cyberConnect.connect(targetAddr);
+
+
+
+export default defineComponent({
+  setup() {
+    
+  },
+})
+</script>
+
+
+
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: '/',
+  lintOnSave: false, 
+  chainWebpack: config => {
+    config.module
+      .rule("graphql")
+      .test(/\.(graphql|gql)$/)
+      .use("graphql-tag/loader")
+      .loader("graphql-tag/loader")
+      .end();
+  }
 })
